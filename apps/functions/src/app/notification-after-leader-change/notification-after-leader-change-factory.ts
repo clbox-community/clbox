cab839667e0445facee943dd1d85ce3ab93fc9fa
Grzegorz Lipecki <glipecki@consdata.com>
2024-04-10 13:13:24 +0200

    Logi wykonania powiadomienia po zmianie chapter leadera

--- apps/functions/src/app/notification-after-leader-change/notification-after-leader-change-factory.ts
+++ apps/functions/src/app/notification-after-leader-change/notification-after-leader-change-factory.ts
@@ -1,17 +1,18 @@
-import {userProfile} from '../slack/fetch-user-profile';
-import {sendSlackMessage} from '../slack/send-slack-message';
+import { userProfile } from '../slack/fetch-user-profile';
+import { sendSlackMessage } from '../slack/send-slack-message';
 
 export const notificationAfterLeaderChangeFactory = (
-  functions: import('firebase-functions').FunctionBuilder,
-  config: Record<string, any>
+    functions: import('firebase-functions').FunctionBuilder,
+    config: Record<string, any>
 ) => functions.firestore.document('team/{team}/user/{user}').onUpdate(
-  async (change, context) => {
-    if (change.after.data().chapterLeader !== change.before.data().chapterLeader) {
-      const slackUser = await userProfile(context.params.user, config.slack.bottoken);
-      await sendSlackMessage(config.slack.bottoken, {
-        channel: `@${slackUser.name}`,
-        text: `Your chapter leader was changed to ${change.after.data().chapterLeader}.`
-      });
+    async (change, context) => {
+        if (change.after.data().chapterLeader !== change.before.data().chapterLeader) {
+            console.log(`Notify user after chapter leader change [user=${context.params.user}, newLeader=${change.after.data().chapterLeader}]`);
+            const slackUser = await userProfile(context.params.user, config.slack.bottoken);
+            await sendSlackMessage(config.slack.bottoken, {
+                channel: `@${slackUser.name}`,
+                text: `Your chapter leader was changed to ${change.after.data().chapterLeader}.`
+            });
+        }
     }
-  }
-)
+);
