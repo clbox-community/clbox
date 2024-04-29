9da693d25538a47f19ec186d4a4ccff63909c168
Grzegorz Lipecki <glipecki@consdata.com>
2024-04-29 08:43:02 +0200

    Ukrywanie ankiet starszych niż 3 miesiące na liście oczekujących ankiet.

--- apps/web-app/src/app/features/changelog/components/changelog.tsx
+++ apps/web-app/src/app/features/changelog/components/changelog.tsx
@@ -23,55 +23,55 @@ const Layout = styled.div`
 
 const changes = [
     {
+        date: '2024-04-29',
+        items: [
+            {summary: 'Lista ankiet domyślnie ukrywa wszystkie ankiety starsze niż 3 miesiące.'},
+        ]
+    },
+    {
         date: '2024-04-26',
         items: [
-            {summary: 'Możliwość tworzenia ankiet zawierających wszystkie pytania na jednej stronie (np. do głosowania nad skill tree)'},
+            {summary: 'Możliwość tworzenia ankiet zawierających wszystkie pytania na jednej stronie (np. do głosowania nad skill tree).'},
         ]
     },
     {
         date: '2024-04-10',
         items: [
-            {summary: 'Sekcja na komentarz do każdego pytania ankiety oceny okresowej'},
-            {summary: 'Szacowanie czasu potrzebnego do ukończenia ankiety okresowej na podstawie średniego czasu odpowiedzi wcześniejszych pytań'},
-            {summary: 'Ekran podsumowania ankiety oceny '},
+            {summary: 'Sekcja na komentarz do każdego pytania ankiety oceny okresowej.'},
+            {summary: 'Szacowanie czasu potrzebnego do ukończenia ankiety okresowej na podstawie średniego czasu odpowiedzi wcześniejszych pytań.'},
+            {summary: 'Ekran podsumowania ankiety oceny.'},
         ]
     },
     {
         date: '2024-01-22',
         items: [
-            {summary: 'Profil użytkownika wyświetlający dane osobowe przechowane w clbox'}
+            {summary: 'Profil użytkownika wyświetlający dane osobowe przechowane w clbox.'}
         ]
     },
     {
         date: '2024-01-19',
         items: [
-            {summary: 'Ankiety oceny okresowej: przeglądanie, tworzenie, wypełnianie oczekujących'}
+            {summary: 'Ankiety oceny okresowej: przeglądanie, tworzenie, wypełnianie oczekujących.'}
         ]
     },
     {
         date: '2023-06-08',
         items: [
-            {summary: 'Zmiana struktury Ścieżki rozwoju technologicznego na dwupoziomową z etykietami obszarów'},
-            {summary: 'Ostrzeżenie o braku implementacji zapisu wartości checkboxów autooceny w kategoriach ścieżki rozwoju technologicznego'},
-            {summary: 'Uzupełnienie polskiego tłumaczenia w filtrze nowych feedbacków'},
+            {summary: 'Zmiana struktury Ścieżki rozwoju technologicznego na dwupoziomową z etykietami obszarów.'},
+            {summary: 'Ostrzeżenie o braku implementacji zapisu wartości checkboxów autooceny w kategoriach ścieżki rozwoju technologicznego.'},
+            {summary: 'Uzupełnienie polskiego tłumaczenia w filtrze nowych feedbacków.'},
         ]
     },
     {
         date: '2023-06-07',
         items: [
-            {summary: 'Przeglądanie Roadmapy uczenia tematów technologicznych z podziałem na obszary i umiejętności'}
+            {summary: 'Przeglądanie Roadmapy uczenia tematów technologicznych z podziałem na obszary i umiejętności.'}
         ]
     },
     {
         date: '2023-05-30',
         items: [
-            {summary: 'Prototyp ekranu ścieżki uczenia dla konkretnej technologii w ramach roadmap / self-study'}
-        ]
-    },
-    {
-        date: '2023-05-25',
-        items: [
-            {summary: 'Aktualizacja bibliotek (Nx, React, React Router, Firebase)'}
+            {summary: 'Prototyp ekranu ścieżki uczenia dla konkretnej technologii w ramach roadmap / self-study.'}
         ]
     },
     {
--- apps/web-app/src/app/features/surveys/surveys.tsx
+++ apps/web-app/src/app/features/surveys/surveys.tsx
@@ -1,29 +1,35 @@
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import {CircularProgress} from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import {Link} from 'react-router-dom';
-import {useEffect, useState} from 'react';
-import {connect, ConnectedProps} from 'react-redux';
-import {AppState} from '../../state/app-state';
-import {firebaseApp} from '../firebase/firebase.app';
-import {OneColumnLayout} from '../layout/one-column-layout';
-import {Survey} from '../survey/model/survey';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { AppState } from '../../state/app-state';
+import { firebaseApp } from '../firebase/firebase.app';
+import { OneColumnLayout } from '../layout/one-column-layout';
+import { Survey } from '../survey/model/survey';
+import Button from '@mui/material/Button';
 
 const firestore = firebaseApp.firestore();
 
 type SurveyItem = Survey & { id: string };
 
-const SurveysView = ({teamId, userId}: ViewProps) => {
+const timeInDay = 24 * 60 * 60 * 1000;
+const cutOffTime = 3 * 30 * timeInDay;
+
+const SurveysView = ({ teamId, userId }: ViewProps) => {
 
     const [surveys, setSurveys] = useState<SurveyItem[]>(undefined);
+    const [showAll, setShowAll] = useState<boolean>(false);
+    const [surveysFiltered, setSurveysFiltered] = useState<SurveyItem[]>(undefined);
 
     useEffect(() => {
         (async () => {
             if (teamId && userId) {
                 const queryResult = await firestore
                     .collection(`/team/${teamId}/survey`)
-                    .where("forUser", "==", userId)
+                    .where('forUser', '==', userId)
                     .get();
                 setSurveys(
                     queryResult.docs.map(doc => ({
@@ -37,29 +43,43 @@ const SurveysView = ({teamId, userId}: ViewProps) => {
         })();
     }, [teamId, userId]);
 
+    useEffect(() => {
+        if (surveys) {
+            setSurveysFiltered(
+                surveys.filter(survey => showAll || (survey.createdTimestamp >= new Date().getTime() - cutOffTime))
+            );
+        } else {
+            setSurveysFiltered(undefined);
+        }
+    }, [surveys, showAll]);
+
     return <OneColumnLayout>
-        {surveys?.length > 0 && surveys.map(survey => <Card key={survey.id}>
-            <Link to={`/s/${teamId}/${survey.id}`} style={{textDecoration: 'none', color: 'inherit'}}>
+        {surveysFiltered?.length > 0 && surveysFiltered.map(survey => <Card key={survey.id}>
+            <Link to={`/s/${teamId}/${survey.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
                 <CardContent>
                     <div>{survey.title}</div>
-                    <div style={{fontSize: '0.8em', fontStyle: 'italic', color: 'gray'}}>
+                    <div style={{ fontSize: '0.8em', fontStyle: 'italic', color: 'gray' }}>
                         <div>Pytania: {survey.pages.length}</div>
                         <div>Utworzona: {survey.created}</div>
                     </div>
                 </CardContent>
             </Link>
         </Card>)}
-        {surveys === undefined && <div style={{textAlign: 'center', marginTop: '64px'}}>
-            <CircularProgress/>
+        {!showAll && surveysFiltered?.length < surveys?.length && <div style={{ marginTop: 16, fontSize: '0.9em', textAlign: 'center' }}>
+            <Button variant={'text'} onClick={() => setShowAll(true)}>pokaż więcej</Button>
+            <div style={{ color: 'gray', fontStyle: 'italic' }}>Ankiety starsze niż 3 miesiące są ukrywane.</div>
+        </div>}
+        {surveys === undefined && <div style={{ textAlign: 'center', marginTop: '64px' }}>
+            <CircularProgress />
         </div>}
         {surveys?.length === 0 && <Card>
-            <CardContent style={{textAlign: 'center'}}>
-                <div><ThumbUpIcon/></div>
+            <CardContent style={{ textAlign: 'center' }}>
+                <div><ThumbUpIcon /></div>
                 <div>Nie masz żadnych niewypełnionych ankiet</div>
             </CardContent>
         </Card>}
     </OneColumnLayout>;
-}
+};
 
 interface ViewProps extends ConnectedProps<typeof connector> {
 }
