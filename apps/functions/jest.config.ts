fcadd9706fff04e2be403acf743b9f8b1d0bdda9
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2024-05-04 08:52:52 +0200

    nx upgrade (#1)

--- apps/functions/jest.config.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-export default {
-    preset: '../../jest.preset.js',
-    coverageDirectory: '../../coverage/apps/functions',
-    globals: {},
-    displayName: 'functions',
-    testEnvironment: 'node',
-};
