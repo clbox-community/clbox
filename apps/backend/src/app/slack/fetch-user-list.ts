8864b0845c33a88e498ec8830170003c9005ea6a
Grzegorz Lipecki <glipecki@consdata.com>
2024-05-09 10:49:18 +0200

    Cache komunikacji ze slack na userProfile i userList.

--- apps/backend/src/app/slack/fetch-user-list.ts
+++ apps/backend/src/app/slack/fetch-user-list.ts
@@ -1,31 +1,42 @@
 import fetch from 'node-fetch';
-import {SlackUserIndex} from "./slack-user-index";
-import {SlackUser} from "./slack-user";
+import { SlackUserIndex } from './slack-user-index';
+import { SlackUser } from './slack-user';
+
+const userListCache: { ttl?: number, result } = {
+    ttl: undefined,
+    result: undefined
+};
 
 export async function userList(bottoken: unknown): Promise<SlackUserIndex | void> {
-  return await fetch(
-    `https://slack.com/api/users.list`,
-    {
-      headers: {
-        Authorization: `Bearer ${bottoken}`,
-        'Content-type': 'application/json'
-      },
-    })
-    .then(res => res.json())
-    .then((res: { members: SlackUser[] }) => {
-      if (res.members) {
-        return res.members;
-      } else {
-        console.log(`Can't fetch slack user list (missing members info) [%o]`, res);
-        throw new Error(`Can't fetch slack user list`);
-      }
-    })
-    .then(
-      (users: SlackUser[]) => users.reduce((index, user) => ({
-          ...index,
-          [user.name]: user
-        }),
-        {} as SlackUserIndex
-      )
-    );
+    if (!userListCache.result || new Date().getTime() > (userListCache.ttl ?? 0)) {
+        console.log(`Building cache entry for user list [previousCache.ttl=${userListCache.ttl ?? 'no-entry'}]`);
+        userListCache.ttl = new Date().getTime() + 1000 * 60 * 10;
+        userListCache.result = await fetch(
+            `https://slack.com/api/users.list`,
+            {
+                headers: {
+                    Authorization: `Bearer ${bottoken}`,
+                    'Content-type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then((res: { members: SlackUser[] }) => {
+                if (res.members) {
+                    return res.members;
+                } else {
+                    console.log(`Can't fetch slack user list (missing members info) [%o]`, res);
+                    throw new Error(`Can't fetch slack user list`);
+                }
+            })
+            .then(
+                (users: SlackUser[]) => users.reduce((index, user) => ({
+                        ...index,
+                        [user.name]: user
+                    }),
+                    {} as SlackUserIndex
+                )
+            );
+        console.log(`New cache entry for user list [cache.ttl=${userListCache.ttl}]`);
+    }
+    return userListCache.result;
 }
--- apps/backend/src/app/slack/fetch-user-profile.ts
+++ apps/backend/src/app/slack/fetch-user-profile.ts
@@ -1,15 +1,28 @@
 import fetch from 'node-fetch';
 
+const userProfileCache: {[key: string]: { ttl?: number, result }} = {
+    ttl: undefined,
+    result: undefined
+};
+
 export async function userProfile(email: string, bottoken: unknown) {
-    return await fetch(
-        `https://slack.com/api/users.lookupByEmail?email=${email}`,
-        {
-            headers: {
-                Authorization: `Bearer ${bottoken}`,
-                'Content-type': 'application/json'
-            },
+    if (!userProfileCache[email] || new Date().getTime() > (userProfileCache[email].ttl ?? 0)) {
+        console.log(`Building cache entry for user profile [email=${email}, previousCache.ttl=${userProfileCache[email]?.ttl ?? 'no-entry'}`);
+        userProfileCache[email] = {
+            ttl: new Date().getTime() + 1000 * 60 * 10,
+            result: await fetch(
+                `https://slack.com/api/users.lookupByEmail?email=${email}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${bottoken}`,
+                        'Content-type': 'application/json'
+                    },
+                }
+            )
+                .then(result => result.json())
+                .then((result: any) => result.user)
         }
-    )
-        .then(result => result.json())
-        .then((result: any) => result.user);
+        console.log(`New cache entry for user list [email=${email}, cache.ttl=${userProfileCache[email].ttl}]`);
+    }
+    return userProfileCache[email];
 }
