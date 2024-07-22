4bf5a020374ece3b5aaa19028a6943e54663d0e9
Grzegorz Lipecki <glipecki@consdata.com>
2024-07-22 11:48:38 +0200

    Usunięcie ograniczeń uprawnień do tworzenia ankiet (#17).

--- apps/web-app/src/app/features/assessment/components/assessments.tsx
+++ apps/web-app/src/app/features/assessment/components/assessments.tsx
@@ -1,15 +1,14 @@
-import {AssessmentsPending} from "./assessments-pending";
-import {OneColumnLayoutWide} from "../../layout/one-column-layout-wide";
-import {AssessmentsActive} from "./assessments-active";
-import {AssessmentsArchive} from "./assessments-archive";
-import {connect, ConnectedProps} from "react-redux";
-import {AppState} from "../../../state/app-state";
-import {useSearchParams} from "react-router-dom";
-import React, {useEffect, useState} from "react";
-import {Snackbar} from "@mui/material";
+import { AssessmentsPending } from './assessments-pending';
+import { OneColumnLayoutWide } from '../../layout/one-column-layout-wide';
+import { AssessmentsActive } from './assessments-active';
+import { connect, ConnectedProps } from 'react-redux';
+import { AppState } from '../../../state/app-state';
+import { useSearchParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Snackbar } from '@mui/material';
 
-export const AssessmentsView = ({isLeader}: ConnectedProps<typeof connector>) => {
-    const [searchParams, setSearchParams ] = useSearchParams();
+export const AssessmentsView = ({}: ConnectedProps<typeof connector>) => {
+    const [searchParams, setSearchParams] = useSearchParams();
     const [toast, setToast] = useState<string | undefined>(undefined);
     useEffect(() => {
         if (searchParams.has('created')) {
@@ -18,7 +17,7 @@ export const AssessmentsView = ({isLeader}: ConnectedProps<typeof connector>) =>
                 replace: true
             });
         }
-    }, []);
+    }, [searchParams, setSearchParams]);
     useEffect(() => {
         if (searchParams.has('finished')) {
             setToast('Dziękujemy za wypełnienie ankiety. Twoje wyniki zostaną opublikowane w ciągu kilku minut.');
@@ -26,34 +25,30 @@ export const AssessmentsView = ({isLeader}: ConnectedProps<typeof connector>) =>
                 replace: true
             });
         }
-    }, []);
+    }, [setToast, setSearchParams]);
     return <OneColumnLayoutWide>
         <Snackbar
-            anchorOrigin={{vertical: 'top', horizontal: 'center'}}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
             open={!!toast}
             autoHideDuration={5000}
             onClose={() => setToast(undefined)}
             message={toast}
             key="toastSnackbar"
         />
-        <div style={{marginBottom: '24px'}}>
-            <AssessmentsPending/>
+        <div style={{ marginBottom: '24px' }}>
+            <AssessmentsPending />
         </div>
-        {isLeader && <div style={{marginBottom: '24px'}}>
-            <AssessmentsActive/>
+        {<div style={{ marginBottom: '24px' }}>
+            <AssessmentsActive />
         </div>}
-        {isLeader && <div>
-            <AssessmentsArchive/>
-        </div>}
-        {/*<div>Otwarte ankiety (lista, status: stworzona, oczekuje na odpowiedzi, oczekuje na podsumowanie)</div>*/}
-        {/*<div>Archiwalne ankiety</div>*/}
-    </OneColumnLayoutWide>
-}
+        {/*{<div>*/}
+        {/*    <AssessmentsArchive/>*/}
+        {/*</div>}*/}
+    </OneColumnLayoutWide>;
+};
 
 const connector = connect(
-    (state: AppState) => ({
-        isLeader: state.profile?.profile?.leader === true,
-    }),
+    (state: AppState) => ({}),
     {}
 );
 
