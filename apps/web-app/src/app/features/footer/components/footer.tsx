fa299aec22231d02d94a9a0ad5096921d23b44d6
Grzegorz Lipecki <glipecki@consdata.com>
2024-07-22 10:50:01 +0200

    Dodanie wersji aplikacji (git sha) do stopki.

--- apps/web-app/src/app/features/footer/components/footer.tsx
+++ apps/web-app/src/app/features/footer/components/footer.tsx
@@ -1,5 +1,19 @@
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const Footer = () => <div>
-    clbox (<Link to="/changelog" style={{textDecoration: 'none', color: 'inherit', fontWeight: '400'}}>changelog</Link>)
+    <Link to="https://github.com/clbox-community/clbox/" style={{ textDecoration: 'none', color: 'inherit', fontWeight: 400 }}>
+        clbox
+    </Link>
+    &nbsp;
+    (
+    <Link to="/changelog" style={{ textDecoration: 'none', color: 'inherit', fontWeight: 300 }}>
+        changelog
+    </Link>
+    &nbsp;
+    /
+    &nbsp;
+    <Link to={'https://github.com/clbox-community/clbox/commit/' + process.env.NX_APP_VERSION} style={{ textDecoration: 'none', color: 'inherit', fontWeight: 300 }}>
+        {process.env.NX_APP_VERSION}
+    </Link>
+    )
 </div>;
