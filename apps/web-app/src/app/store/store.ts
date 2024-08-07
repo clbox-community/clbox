c50d2b4c68caabad7ccb2540e7580ededed07561
Grzegorz Lipecki <glipecki@consdata.com>
2024-08-07 12:07:50 +0200

    Removes Sentry deps

--- apps/web-app/src/app/store/store.ts
+++ apps/web-app/src/app/store/store.ts
@@ -1,49 +1,12 @@
-import {AnyAction, configureStore} from '@reduxjs/toolkit';
-import {epicMiddleware} from './epic-middleware';
-import {rootEpic} from './root-epic';
-import {rootReducer} from './root-reducer';
-import * as Sentry from "@sentry/react";
-import {AppState} from '../state/app-state';
-
-const sentryReduxEnhancer = Sentry.createReduxEnhancer({
-    stateTransformer: state => {
-        return {
-            ...state,
-            inbox: {
-                messages: undefined
-            },
-            sent: {
-                messages: undefined
-            }
-        } as AppState;
-    },
-    actionTransformer(action: AnyAction): AnyAction | null {
-        if (action.type === 'InboxFetched') {
-            return {
-                ...action,
-                payload: {
-                    ...action.payload,
-                    messages: undefined
-                }
-            }
-        }
-        if (action.type === 'SentFetched') {
-            return {
-                ...action,
-                payload: {
-                    ...action.payload,
-                    messages: undefined
-                }
-            }
-        }
-        return action;
-    }
-});
+import { configureStore } from '@reduxjs/toolkit';
+import { epicMiddleware } from './epic-middleware';
+import { rootEpic } from './root-epic';
+import { rootReducer } from './root-reducer';
 
 export const store = configureStore({
     reducer: rootReducer(),
     middleware: [epicMiddleware],
-    enhancers: [sentryReduxEnhancer]
+    enhancers: []
 });
 
 epicMiddleware.run(rootEpic);
