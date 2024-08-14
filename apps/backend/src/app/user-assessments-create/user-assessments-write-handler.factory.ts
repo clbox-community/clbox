db37755ef5452747cceeb83f2ce94880ff42c454
Grzegorz Lipecki <glipecki@consdata.com>
2024-08-14 12:17:39 +0200

    Fixes: user assessment recreated after previous finished for existing assessors

--- apps/backend/src/app/user-assessments-create/user-assessments-write-handler.factory.ts
+++ apps/backend/src/app/user-assessments-create/user-assessments-write-handler.factory.ts
@@ -1,20 +1,26 @@
 import { Assessment, UserAssessment, UserAssessmentRef } from 'assessment-model';
 
-export const userAssessmentsCreateHandlerFactory = (
+export const userAssessmentsWriteHandlerFactory = (
     functions: import('firebase-functions').FunctionBuilder,
     firebase: typeof import('firebase-admin')
 ) => functions.firestore.document('team/{team}/assessment/{assessment}').onWrite(
     async (change, context) => {
-        if (!change.after) {
+        if (!change.after.exists) {
             console.log(`Skipping user assessment documents when assessment deleted`);
             // todo: if we will ever flush user assessment documents we should store them on the side to not lose user data
             return;
         }
 
         const db = firebase.firestore();
+        const prevAssessment = change.before.data() as Assessment | undefined;
         const assessment = change.after.data() as Assessment;
-        const assessors: string[] = assessment.assessors ?? [];
+        const assessors: string[] = assessment?.assessors ?? [];
         for (const assessor of assessors) {
+            if (prevAssessment?.assessors?.includes(assessor)) {
+                continue;
+            }
+            console.log(`Updating assessment documents for added assessor: ${assessor}`);
+
             const userAssessment: UserAssessment = {
                 ...assessment,
                 assessmentId: change.after.id,
@@ -27,6 +33,7 @@ export const userAssessmentsCreateHandlerFactory = (
                 finished: false
             };
             const assessmentDocRef = db.doc(`team/${context.params.team}/assessment/${change.after.id}/result/${assessor}`);
+            console.log(`Updating assessment result document for user [data=${JSON.stringify(userAssessment)}, ref=${assessmentDocRef.path}]`)
             await assessmentDocRef.set(userAssessment);
 
             const userAssessmentRef: UserAssessmentRef = {
@@ -38,10 +45,9 @@ export const userAssessmentsCreateHandlerFactory = (
                 deadline: assessment.deadline,
                 finished: false
             };
-            console.log(`Creating user assessment for [data=${JSON.stringify(userAssessment)}, ref=${JSON.stringify(userAssessmentRef)}]`);
             const userAssessmentDocRef = db.doc(`team/${context.params.team}/user/${assessor}/user-assessment-pending/${change.after.id}`);
+            console.log(`Updating user assessment for [data=${JSON.stringify(userAssessmentRef)}, path=${userAssessmentDocRef.path}]`);
             await userAssessmentDocRef.set(userAssessmentRef);
-            console.log(`User assessment created with [ref=${userAssessmentDocRef.path}, assessment=${assessmentDocRef.path}]`);
         }
     }
 );
