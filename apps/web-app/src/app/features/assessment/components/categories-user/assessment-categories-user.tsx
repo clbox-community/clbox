648af9c40cf5834890bada9a08027241056523eb
Grzegorz Lipecki <glipecki@consdata.com>
2025-02-28 09:07:31 +0100

    #49 Możliwość określenia zbioru pytań oceny okresowej dla każdej osoby (oznaczenia pytań jako zweryfikowanych lub ignorowanych)

--- apps/web-app/src/app/features/assessment/components/categories-user/assessment-categories-user.tsx
+++ apps/web-app/src/app/features/assessment/components/categories-user/assessment-categories-user.tsx
@@ -4,7 +4,7 @@ import { AppState } from '../../../../state/app-state';
 import { OneColumnLayoutUltraWide } from '../assessment-result';
 import { useUserProfile } from '../../../user/firestore/use-user-profile';
 import { Spinner } from '../../../../ui/spinner/spinner';
-import { categories, Question } from '@clbox/assessment-survey';
+import { categories, Category, Question } from '@clbox/assessment-survey';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import { FormControl } from '@mui/material';
@@ -14,6 +14,7 @@ import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { UserAssessmentVerification, UserAssessmentVerifiedCategories, UserAssessmentVerifiedCategory } from '../../model/user-assessment-verified-categories';
 import { firebaseApp } from '../../../firebase/firebase.app';
+import { UserProfile } from 'user-profile-model';
 
 function verificationColor(verifiedStatus: UserAssessmentVerification) {
     switch (verifiedStatus) {
@@ -33,9 +34,41 @@ function verificationColor(verifiedStatus: UserAssessmentVerification) {
 const CategoryQuestionsRow = styled.div``;
 
 const CategoryQuestionRow = styled.div`
+    margin: 0 8px;
+
     &:hover {
         outline: 1px dashed gray;
     }
+`;
+
+const CategoryQuestionColumn = styled.div`
+    display: flex;
+    align-items: center;
+`;
+const CategoryQuestionTextColumn = styled(CategoryQuestionColumn)`
+    flex: 1;
+`;
+const CategoryQuestionTextSeniorityColumn = styled(CategoryQuestionColumn)`
+    width: 80px;
+`;
+const CategoryQuestionTextIdColumn = styled(CategoryQuestionColumn)`
+    width: 80px;
+`;
+const CategoryQuestionTextAssessmentColumn = styled(CategoryQuestionColumn)`
+    width: 170px
+`;
+const CategoryQuestionAuditRow = styled.div`
+    font-size: .9em;
+    color: gray;
+    font-style: italic;
+    text-align: right;
+`;
+
+const CategoryRowPanel = styled.div`
+    margin-bottom: 12px;
+`;
+const CategoryRowName = styled.div`
+    font-weight: bold;
 `;
 
 function filterCategories(filter: UserAssessmentVerification, verifiedCategories: { [p: string]: UserAssessmentVerifiedCategory }) {
@@ -61,6 +94,102 @@ function filterCategories(filter: UserAssessmentVerification, verifiedCategories
     }
 }
 
+const QuestionAssessmentVerificationSelect = ({ value, onChange }: { value: UserAssessmentVerification, onChange: (value: UserAssessmentVerification) => void }) => {
+    return <FormControl sx={{ m: 1, width: '160px' }} size="small">
+        <Select
+            value={value}
+            onChange={event => onChange(event.target.value as UserAssessmentVerification)}
+        >
+            <MenuItem value={UserAssessmentVerification.Ask}>Pytaj</MenuItem>
+            <MenuItem value={UserAssessmentVerification.Verified}>Potwierdzone</MenuItem>
+            <MenuItem value={UserAssessmentVerification.Skip}>Nie dotyczy</MenuItem>
+            {/*<MenuItem value={VerifiedStatus.Verify}>Potwierdź</MenuItem>*/}
+        </Select>
+    </FormControl>;
+};
+
+const UserAssessmentVerificationFilter = ({ value, onChange }: { value: UserAssessmentVerification | undefined, onChange: (value: UserAssessmentVerification | undefined) => void }) => {
+    return <ButtonGroup variant="text" size="small" color="inherit">
+        <Button
+            sx={{ fontWeight: value === undefined ? 'bold' : 'inherit' }}
+            onClick={() => onChange(undefined)}>
+            wszystkie
+        </Button>
+        <Button
+            sx={{ fontWeight: value === UserAssessmentVerification.Ask ? 'bold' : 'inherit' }}
+            onClick={() => onChange(UserAssessmentVerification.Ask)}>
+            aktywne
+        </Button>
+        <Button
+            sx={{ fontWeight: value === UserAssessmentVerification.Verified ? 'bold' : 'inherit' }}
+            onClick={() => onChange(UserAssessmentVerification.Verified)}>
+            potwierdzone
+        </Button>
+        <Button
+            sx={{ fontWeight: value === UserAssessmentVerification.Skip ? 'bold' : 'inherit' }}
+            onClick={() => onChange(UserAssessmentVerification.Skip)}>
+            ignorowane
+        </Button>
+    </ButtonGroup>;
+};
+
+function fetchUserVerifiedCategories(teamId: string, user: string, setVerifiedCategories: (value: (((prevState: UserAssessmentVerifiedCategories) => UserAssessmentVerifiedCategories) | UserAssessmentVerifiedCategories)) => void) {
+    firebaseApp.firestore()
+        .doc(`/team/${teamId}/user/${user}/data/assessment-categories`)
+        .get()
+        .then(snapshot => {
+            setVerifiedCategories(snapshot.data() || {});
+        });
+}
+
+function updateUserVerifiedCategories(teamId: string, user: string, question: Question, updatedVerificationStatus: { status: UserAssessmentVerification; author: string; date: number }) {
+    firebaseApp.firestore()
+        .doc(`/team/${teamId}/user/${user}/data/assessment-categories`)
+        .set({
+            [question.id]: updatedVerificationStatus
+        }, {
+            merge: true
+        })
+        .catch(error => {
+            console.error(error);
+            alert('Nie udało się zapisać, spójrz w logi i zgłoś problem ;)');
+        });
+}
+
+const CategoryRow = ({ category, verifiedCategories, userProfile, updateVerificationStatus }: { category: Category, verifiedCategories: UserAssessmentVerifiedCategories, userProfile: UserProfile, updateVerificationStatus: (question: Question, status: UserAssessmentVerification) => void }) => {
+    return <CategoryRowPanel>
+        <CategoryRowName>
+            {category.name} ({category.id})
+        </CategoryRowName>
+        <CategoryQuestionsRow>
+            {category.questions.map(
+                question => <CategoryQuestionRow key={question.id}>
+                    <div style={{ display: 'flex', color: verificationColor(verifiedCategories[question.id]?.status ?? UserAssessmentVerification.Ask) }}>
+                        <CategoryQuestionTextColumn>
+                            {question.text3rd ? question.text3rd?.[userProfile.textForm ?? 'm'] : question.text1st?.[userProfile.textForm ?? 'm']}
+                        </CategoryQuestionTextColumn>
+                        <CategoryQuestionTextSeniorityColumn>
+                            {question.seniority}
+                        </CategoryQuestionTextSeniorityColumn>
+                        <CategoryQuestionTextIdColumn>
+                            {question.id}
+                        </CategoryQuestionTextIdColumn>
+                        <CategoryQuestionTextAssessmentColumn>
+                            <QuestionAssessmentVerificationSelect
+                                value={verifiedCategories[question.id]?.status ?? UserAssessmentVerification.Ask}
+                                onChange={value => updateVerificationStatus(question, value)}
+                            />
+                        </CategoryQuestionTextAssessmentColumn>
+                    </div>
+                    {verifiedCategories[question.id] && <CategoryQuestionAuditRow>
+                        {verifiedCategories[question.id]?.author} {verifiedCategories[question.id]?.date ? new Date(verifiedCategories[question.id]?.date).toISOString() : undefined}
+                    </CategoryQuestionAuditRow>}
+                </CategoryQuestionRow>
+            )}
+        </CategoryQuestionsRow>
+    </CategoryRowPanel>;
+};
+
 export const AssessmentCategoriesUserView = ({ teamId, loggedUser }: ConnectedProps<typeof connector>) => {
     const { user } = useParams<{ user: string | undefined }>();
     const userProfile = useUserProfile(teamId, user);
@@ -69,12 +198,7 @@ export const AssessmentCategoriesUserView = ({ teamId, loggedUser }: ConnectedPr
 
     useEffect(() => {
         if (teamId && user) {
-            firebaseApp.firestore()
-                .doc(`/team/${teamId}/user/${user}/data/assessment-categories`)
-                .get()
-                .then(snapshot => {
-                    setVerifiedCategories(snapshot.data() || {});
-                });
+            fetchUserVerifiedCategories(teamId, user, setVerifiedCategories);
         }
     }, [teamId, user]);
 
@@ -90,13 +214,7 @@ export const AssessmentCategoriesUserView = ({ teamId, loggedUser }: ConnectedPr
                     ...current, [question.id]: updatedVerificationStatus
                 })
             );
-            firebaseApp.firestore()
-                .doc(`/team/${teamId}/user/${user}/data/assessment-categories`)
-                .set({
-                    [question.id]: updatedVerificationStatus
-                }, {
-                    merge: true
-                })
+            updateUserVerifiedCategories(teamId, user, question, updatedVerificationStatus);
         },
         [teamId, user, loggedUser]
     );
@@ -108,50 +226,20 @@ export const AssessmentCategoriesUserView = ({ teamId, loggedUser }: ConnectedPr
         {userProfile && verifiedCategories && <>
             <div style={{ marginBottom: '16px', fontSize: '1.2em', borderBottom: '1px solid gray' }}>{userProfile?.display_name}</div>
             <div style={{ textAlign: 'right' }}>
-                <ButtonGroup variant="text" size="small" color="inherit">
-                    <Button sx={{ fontWeight: filter === undefined ? 'bold' : 'inherit' }} onClick={() => setFilter(undefined)}>wszystkie</Button>
-                    <Button sx={{ fontWeight: filter === UserAssessmentVerification.Ask ? 'bold' : 'inherit' }} onClick={() => setFilter(UserAssessmentVerification.Ask)}>aktywne</Button>
-                    <Button sx={{ fontWeight: filter === UserAssessmentVerification.Verified ? 'bold' : 'inherit' }}
-                            onClick={() => setFilter(UserAssessmentVerification.Verified)}>potwierdzone</Button>
-                    <Button sx={{ fontWeight: filter === UserAssessmentVerification.Skip ? 'bold' : 'inherit' }} onClick={() => setFilter(UserAssessmentVerification.Skip)}>ignorowane</Button>
-                </ButtonGroup>
+                <UserAssessmentVerificationFilter
+                    value={filter}
+                    onChange={setFilter}
+                />
             </div>
             <div>
                 {filteredCategories.map(
-                    category => <div key={category.id} style={{ marginBottom: '12px' }}>
-                        <div style={{ fontWeight: 'bold' }}>{category.name} ({category.id})</div>
-                        <CategoryQuestionsRow>
-                            {category.questions.map(
-                                question => <CategoryQuestionRow key={question.id}
-                                                                 style={{ margin: '0 8px' }}>
-                                    <div style={{ display: 'flex', color: verificationColor(verifiedCategories[question.id]?.status ?? UserAssessmentVerification.Ask) }}>
-                                        <div style={{ flex: 1, display: 'flex', alignItems: 'center' }}>
-                                            {question.text3rd ? question.text3rd?.[userProfile.textForm ?? 'm'] : question.text1st?.[userProfile.textForm ?? 'm']}
-                                        </div>
-                                        <div style={{ width: '80px', display: 'flex', alignItems: 'center' }}>
-                                            {question.id}
-                                        </div>
-                                        <div style={{ width: '170px' }}>
-                                            <FormControl sx={{ m: 1, width: '160px' }} size="small">
-                                                <Select
-                                                    value={verifiedCategories[question.id]?.status ?? UserAssessmentVerification.Ask}
-                                                    onChange={event => updateVerificationStatus(question, event.target.value as UserAssessmentVerification)}
-                                                >
-                                                    <MenuItem value={UserAssessmentVerification.Ask}>Pytaj</MenuItem>
-                                                    <MenuItem value={UserAssessmentVerification.Verified}>Potwierdzone</MenuItem>
-                                                    <MenuItem value={UserAssessmentVerification.Skip}>Nie dotyczy</MenuItem>
-                                                    {/*<MenuItem value={VerifiedStatus.Verify}>Potwierdź</MenuItem>*/}
-                                                </Select>
-                                            </FormControl>
-                                        </div>
-                                    </div>
-                                    {verifiedCategories[question.id] && <div style={{ fontSize: '0.9em', color: 'gray', fontStyle: 'italic', textAlign: 'right' }}>
-                                        {verifiedCategories[question.id]?.author} {verifiedCategories[question.id]?.date ? new Date(verifiedCategories[question.id]?.date).toISOString() : undefined}
-                                    </div>}
-                                </CategoryQuestionRow>
-                            )}
-                        </CategoryQuestionsRow>
-                    </div>
+                    category => <CategoryRow
+                        key={category.id}
+                        category={category}
+                        userProfile={userProfile}
+                        verifiedCategories={verifiedCategories}
+                        updateVerificationStatus={updateVerificationStatus}
+                    />
                 )}
             </div>
         </>}
